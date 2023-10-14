2b10362b6433b474c4a95fe3972f716d7d596259
tanmay2133 <tanmaynaik2003@gmail.com>
2023-10-14 20:26:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+function Portfolio() {
+  const name = 'Tanmay Naik';
+  const age = 20;
+  const dob = '21/03/2003';
+  const native = 'Surat, Gujarat';
+  const school = 'Reliance Foundation School, Surat';
+  const college = 'Vellore Institute of Technology, Chennai Campus';
+  const cgpa = 8.26;
+  const skills = 'Proficient in multiple programming languages, can build websites';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header>
+        <h1>{name}</h1>
+        <p>Age: {age}</p>
+        <p>DOB: {dob}</p>
+        <p>Native: {native}</p>
       </header>
+      <main>
+        <section>
+          <h2>Education</h2>
+          <p>School: {school}</p>
+          <p>College: {college}</p>
+          <p>CGPA: {cgpa}</p>
+        </section>
+        <section>
+          <h2>Skills</h2>
+          <p>{skills}</p>
+        </section>
+      </main>
     </div>
   );
 }
 
-export default App;
+export default Portfolio;
